2faf234a3e1a9a039b3f677a6c738135682b4ec0
Immersor-git <62318692+Immersor-git@users.noreply.github.com>
2024-10-06 23:47:01 -0700

    Checkerboard

---
+++ checkerboard.gd
@@ -0,0 +1,53 @@
+@tool
+extends Node3D
+
+@export var size : Vector2 = Vector2(0,0):
+	set(value):
+		size = value
+		update_board(value)
+		
+var WhiteSquare : CSGMesh3D
+var BlackSquare : CSGMesh3D
+var generated = false
+var planes = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	WhiteSquare = get_node("WhiteSquare")
+	BlackSquare = get_node("BlackSquare")
+	generated = true
+	update_board(size)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func update_board (size):
+	if generated == false: return
+	print("Updated size")
+	var boardSize = scale
+	for square : CSGMesh3D in planes:
+		square.queue_free()
+	planes.clear()
+	var alternate = 0
+	for x in size.x - 1:
+		for y in size.y - 1:
+			if alternate == 0:
+				var square : CSGMesh3D = WhiteSquare.duplicate()
+				self.add_child(square)
+				square.owner = self
+				square.position = Vector3(x,0,y)
+				square.visible = true
+				self.add_child(square)
+				planes.append(square)
+			else:
+				var square : CSGMesh3D = BlackSquare.duplicate()
+				self.add_child(square)
+				square.owner = self
+				square.position = Vector3(x,0,y)
+				square.visible = true
+				
+				planes.append(square)
+				
+			alternate = (alternate + 1) % 2
+		
+	
