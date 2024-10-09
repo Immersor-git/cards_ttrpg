6adc6bb716eb598cc91bd79188f036d78e821baf
asher <momomitchellsmith@gmail.com>
2024-10-08 18:18:58 -0700

    add scene for inkbird's opal, access a card's components in card.gd, make folder for each spell

--- cards/card.gd
+++ cards/card.gd
@@ -4,10 +4,15 @@ extends Node3D
 
 @export var card: AbilityCard
 @onready var cardTitle = $CardTitle
+var components : Array[Node]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	updateCardTitle()
+	var children : Array = self.get_children()
+	for child in children:
+		if child.name == "Components":
+			components = child.get_children()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
