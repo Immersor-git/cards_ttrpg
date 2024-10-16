4ac6e84e13d1d0dae449ee48e6b091f367fe2cb4
G0ldenSp00n <71467406+G0ldenSp00n@users.noreply.github.com>
2024-10-15 18:13:56 -0700

    feat: movement-overhaul (#2)
    * add board square highlighting, WIP movement overhaul
    
    * WIP rewriting getPathsToPossibleSquares
    
    * highlight in-range squares using recursion, but not breadth-first
    
    * finish breadth-first basic move logic
    
    * fix movement bugs, may need cleaning up later, client can't move yet, add better movement tweening
    
    * make movement prioritize cardinal directions, fix bug causing movement to be 1 short
    
    * fix: remove invisible obstacle at 6, 6
    
    * feat: add client support for movement overhaul
    
    ---------
    
    Co-authored-by: asher <momomitchellsmith@gmail.com>

--- Caster/Caster.gd
+++ Caster/Caster.gd
@@ -13,14 +13,16 @@ extends Node3D
 @export var camera: Camera3D
 @export var is_casters_turn := false
 @export var casterMaterial: Array[Material]
+@export var currentState: Enums.PlayerState = Enums.PlayerState.SITTING_NEUTRAL
 
 var basicMovesAvailable: int = 0
-var currentState: Enums.PlayerState = Enums.PlayerState.SITTING_NEUTRAL
 var caster_id := 1:
 	set(id):
 		caster_id = id
+		$PlayerInput.set_multiplayer_authority(id)
 var team_id := 0
 var isReadyToDraw := false
+var arrOfInvalidSquares : Array[Vector2] = []
 
 @onready var board_piece = $MeshInstance3D
 @onready var seated_neutral = $SeatedNeutral
@@ -68,7 +70,7 @@ func set_player_number(player_num: int):
 	set_board_piece_color(player_num)
 	if player_num == 1:
 		self.global_rotation_degrees = Vector3(0, 180, 0)
-		self.boardPosition = Vector2(7, 7)
+		self.boardPosition = Vector2(0, 0)
 	elif player_num == 2:
 		self.global_rotation_degrees = Vector3(0, 90, 0)
 		self.boardPosition = Vector2(0, 7)
@@ -111,18 +113,89 @@ func discard_mana(manaPath: NodePath):
 
 func _client_try_move(targetSquare: Vector2):
 	try_move.rpc(targetSquare)
+	if currentState == Enums.PlayerState.OBSERVING_BOARD && targetSquare == boardPosition:
+		currentState = Enums.PlayerState.MOVING_PIECE
+		updateArrOfInvalidSquares()
+		dictOfPreviousSquaresGlobalTemp = getPathsToPossibleSquares(arrOfInvalidSquares)
+	elif currentState == Enums.PlayerState.MOVING_PIECE:
+		if dictOfPreviousSquaresGlobalTemp.has(str(targetSquare)):
+				board.clearHighlights()
+				if !multiplayer.is_server():
+					var pathToTargetSquare := findPathToSquare(targetSquare, dictOfPreviousSquaresGlobalTemp)
+					await get_tree().create_timer((0.35 + 0.18) * pathToTargetSquare.size()).timeout
+					updateArrOfInvalidSquares()
+					dictOfPreviousSquaresGlobalTemp = getPathsToPossibleSquares(arrOfInvalidSquares)
 
+var dictOfPreviousSquaresGlobalTemp: Dictionary
 @rpc("any_peer", "call_local", "reliable")
 func try_move(targetSquare: Vector2):
 	if isCastersTurn():
-		var distanceVector: Vector2 = abs(targetSquare - boardPosition)
-		var movesNeeded = max(distanceVector.x, distanceVector.y)
-		if currentState == Enums.PlayerState.MOVING_PIECE:
-			if movesNeeded <= basicMovesAvailable:
-				boardPosition = targetSquare
-				var tween = create_tween()
-				tween.tween_property(board_piece, "global_position", board.boardToWorldCoord(boardPosition), (distanceVector.x + distanceVector.y) * 0.25)
-				basicMovesAvailable -= movesNeeded
+		if currentState == Enums.PlayerState.OBSERVING_BOARD && targetSquare == boardPosition:
+			currentState = Enums.PlayerState.MOVING_PIECE
+			updateArrOfInvalidSquares()
+			dictOfPreviousSquaresGlobalTemp = getPathsToPossibleSquares(arrOfInvalidSquares)
+		elif currentState == Enums.PlayerState.MOVING_PIECE:
+			if basicMovesAvailable >= 1 && dictOfPreviousSquaresGlobalTemp.has(str(targetSquare)):
+				var pathToTargetSquare := findPathToSquare(targetSquare, dictOfPreviousSquaresGlobalTemp)
+				var speedModifier := pathToTargetSquare.size()
+				while pathToTargetSquare.size() > 0:
+					var nextStep: Vector2 = pathToTargetSquare.pop_back()
+					if nextStep != boardPosition:
+						boardPosition = nextStep
+						var tween = create_tween()
+						tween.tween_property(board_piece, "global_position", board.boardToWorldCoord(boardPosition), 0.35)
+						basicMovesAvailable -= 1
+						await tween.finished
+						await get_tree().create_timer(.18).timeout
+				updateArrOfInvalidSquares()
+				dictOfPreviousSquaresGlobalTemp = getPathsToPossibleSquares(arrOfInvalidSquares)
+
+func updateArrOfInvalidSquares():
+	arrOfInvalidSquares.clear()
+	for caster in getCastersInRadius(7):
+		arrOfInvalidSquares.append(caster.boardPosition)
+
+func getNeighbors(startingSquare: Vector2)-> Array[Vector2]:
+	var validAdjacentSquares : Array[Vector2] = []
+	var cardinalNeighbors : Array[Vector2] = []
+	var diagNeighbors :Array[Vector2] = []
+	for targetRelativeX in range(-1, 2):
+		for targetRelativeY in range(-1, 2):
+			var adjacentSquare := startingSquare + Vector2(targetRelativeX, targetRelativeY)
+			if adjacentSquare.x >= 0 && adjacentSquare.x <= 7 && adjacentSquare.y >= 0 && adjacentSquare.y <= 7:
+				if adjacentSquare.x == startingSquare.x || adjacentSquare.y == startingSquare.y:
+					cardinalNeighbors.append(adjacentSquare)
+				else:
+					diagNeighbors.append(adjacentSquare)
+	validAdjacentSquares.append_array(cardinalNeighbors)
+	validAdjacentSquares.append_array(diagNeighbors)
+	return validAdjacentSquares
+
+func findPathToSquare(targetSquare: Vector2, dictOfPreviousSquares: Dictionary)-> Array[Vector2]:
+	var pathToSquare: Array[Vector2] = [targetSquare]
+	var currentSquare := targetSquare
+	while currentSquare != boardPosition:
+		currentSquare = dictOfPreviousSquares[str(currentSquare)]
+		pathToSquare.append(currentSquare)
+	return pathToSquare
+
+func getPathsToPossibleSquares(arrOfInvalidSquares)-> Dictionary:
+	var queue: Array[Vector2] = [boardPosition]
+	#every key in this dictionary will represent a square. It's value will represent the square that you can enter that square FROM.
+	var dictOfPreviousSquares: Dictionary
+	var currentSquare := boardPosition
+	while queue.size() != 0:
+		currentSquare = queue.pop_front()
+		if findPathToSquare(currentSquare, dictOfPreviousSquares).size() <= basicMovesAvailable:
+			var neighbors = getNeighbors(currentSquare)
+			for nextSquare in neighbors:
+				if !arrOfInvalidSquares.has(nextSquare):
+					queue.append(nextSquare)
+					arrOfInvalidSquares.append(nextSquare)
+					dictOfPreviousSquares[str(nextSquare)] = currentSquare
+					if !multiplayer.is_server() || caster_id == 1:
+						board.highlightSquare(nextSquare)
+	return dictOfPreviousSquares
 
 func spawnHand():
 	if multiplayer.is_server():
@@ -152,23 +225,21 @@ func draw():
 			var drawnCards := deck.drawNCards(6-bank.manaPool.size(), self)
 			bank.addManaCards(drawnCards)
 
-@rpc("any_peer", "call_local", "reliable")
-func updateState(state: Enums.PlayerState):
-	currentState = state
-
 func updateCurrentState():
 	if multiplayer.get_unique_id() == caster_id:
 		if Input.is_action_just_pressed("move_back"):
 			currentState = Enums.PlayerState.SITTING_NEUTRAL
+			board.clearHighlights()
 			var tween = create_tween()
 			tween.parallel().tween_property(camera, "global_position", seated_neutral.global_position, 0.25)
 			tween.parallel().tween_property(camera, "global_rotation_degrees", Vector3(-24.2, camera.global_rotation_degrees.y, 0), 0.25)
 		if Input.is_action_just_pressed("move_forward") && currentState == Enums.PlayerState.SITTING_NEUTRAL:
-			currentState = Enums.PlayerState.MOVING_PIECE
+			currentState = Enums.PlayerState.OBSERVING_BOARD
 			var tween = create_tween()
 			tween.parallel().tween_property(camera, "global_position", observing_board.global_position, 0.25)
 			tween.parallel().tween_property(camera, "global_rotation_degrees", Vector3(-50, camera.global_rotation_degrees.y, 0), 0.25 )
-		updateState.rpc(currentState)
+		if basicMovesAvailable == 0 && currentState == Enums.PlayerState.MOVING_PIECE:
+			currentState = Enums.PlayerState.OBSERVING_BOARD
 
 func _client_pass_turn(card_pass: bool):
 	passTurn.rpc(card_pass)
@@ -184,6 +255,7 @@ func _on_pass_turn_collider_input_event(camera, event, event_position, normal, s
 	if event is InputEventMouseButton && event.is_action_pressed("left_click"):
 		_client_pass_turn(false)
 
+## Returns an array of other casters that are within the given radius of this caster. Results include this caster. 
 func getCastersInRadius(radiusInclusive: int) -> Array[Caster]:
 	var listOfCasters = get_tree().get_current_scene().get_node("World/Casters").get_children()
 	var listOfCastersInRadius : Array[Caster] = []
